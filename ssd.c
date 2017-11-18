60d764dba0e470cd6596699077b281b2267e08ed
mishaszu <mishaszu@gmail.com>
2017-11-18 19:03:37 +0100

    first working version

--- ssd.c
+++ ssd.c
@@ -1,3 +1,216 @@
-int main() {
-  return 0;
+#include <Arduino.h>
+
+void display_none();
+void display_zero();
+void display_one();
+void display_two();
+void display_three();
+void display_four();
+void display_five();
+void display_six();
+void display_seven();
+void display_eight();
+void display_nine();
+
+void display_com1();
+void display_com2();
+void display_com3();
+void display_com4();
+
+void choose_number(int num);
+
+void initPins(int *arr) {
+  for (int i = 0; i < 13; i++) {
+    pinMode(arr[i], OUTPUT);
+  }
+}
+
+void displayNumber(int num) {
+  int digit[4] = {
+    10, 10, 10, 10
+  };
+  int i = 3;
+  while (num > 0) {
+    digit[i] = num % 10;
+    num /= 10;
+    i--;
+  }
+  for (int i = 0; i < 4; i++) {
+    if (i == 0) {
+      display_com1();
+    } else if(i == 1) {
+      display_com2();
+    } else if(i == 2) {
+      display_com3();
+    } else if(i == 3) {
+      display_com4();
+    }
+    if (digit[i]) {
+      choose_number(digit[i]);
+    }
+    delay(3);
+  }
+}
+
+void choose_number(int num) {
+  if (num == 0) {
+    display_zero();
+  } else if (num == 1) {
+    display_one();
+  } else if (num == 2) {
+    display_two();
+  } else if (num == 3) {
+    display_three();
+  } else if (num == 4) {
+    display_four();
+  } else if (num == 5) {
+    display_five();
+  } else if (num == 6) {
+    display_six();
+  } else if (num == 7) {
+    display_seven();
+  } else if (num == 8) {
+    display_eight();
+  } else if (num == 9) {
+    display_nine();
+  } else {
+    display_none();
+  }
+}
+
+void display_none() {
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+}
+
+void display_zero() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+}
+
+void display_one () {
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+}
+void display_two() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_three() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_four() {
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_five() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_six() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_seven() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+}
+void display_eight() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_nine() {
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+void display_com1() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+}
+void display_com2() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+}
+void display_com3() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+}
+void display_com4() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
 }
---
+++ ssd.h
@@ -0,0 +1,9 @@
+#ifndef ssd_h_
+#define ssd_h_
+
+#include <Arduino.h>
+
+void initPins(int *arr);
+void displayNumber(int num);
+
+#endif
